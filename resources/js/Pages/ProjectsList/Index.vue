1ab588db78178d7f6c8813f9310901e9e894ceee
Daniil <kononov.miromanov@gmail.com>
2023-07-18 13:23:12 +0300

    2. first version of the table + nav panel for editing.

--- resources/js/Pages/ProjectsList/Index.vue
+++ resources/js/Pages/ProjectsList/Index.vue
@@ -1,28 +1,107 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { useForm, Head } from '@inertiajs/vue3';
- 
-const form = useForm({
-    message: '',
-});
-</script>
- 
 <template>
-    <Head title="ProjectsList" />
- 
+    <Head title="Projects" />
+
     <AuthenticatedLayout>
-        <div class="max-w-xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('projects.store'), { onSuccess: () => form.reset() })">
-                <textarea
-                    v-model="form.message"
-                    placeholder="What's on your mind?"
-                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                ></textarea>
-                <InputError :message="form.errors.message" class="mt-2" />
-                <PrimaryButton class="mt-4">Create project</PrimaryButton>
-            </form>
+        <!-- <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Projects List
+            </h2>
+        </template> -->
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table
+                        class="w-full text-sm text-left text-black dark:text-black"
+                    >
+                        <thead
+                            class="text-xs text-black uppercase bg-gray-50 dark:bg-gray-50"
+                        >
+                            <tr>
+                                <th scope="col" class="p-4"></th>
+                                <th scope="col" class="px-6 py-3">Title</th>
+                                <th scope="col" class="px-6 py-3">Image</th>
+                                <th scope="col" class="px-6 py-3">
+                                    Description
+                                </th>
+                                <th scope="col" class="px-6 py-3">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                class="bg-white border-b"
+                            >
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        <input
+                                            id="checkbox-table-search-1"
+                                            type="checkbox"
+                                            class="w-4 h-4 bg-gray-100 rounded"
+                                        />
+                                        <label
+                                            for="checkbox-table-search-1"
+                                            class="sr-only"
+                                            >checkbox</label
+                                        >
+                                    </div>
+                                </td>
+                                <th
+                                    scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                                >
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">Silver</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">
+                                    <a
+                                        href="#"
+                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                        >Edit</a
+                                    >
+                                </td>
+                            </tr>
+                            <tr
+                                class="bg-white border-black dark:border-gray-700"
+                            >
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        <input
+                                            id="checkbox-table-search-2"
+                                            type="checkbox"
+                                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded dark:border-gray-600"
+                                        />
+                                        <label
+                                            for="checkbox-table-search-2"
+                                            class="sr-only"
+                                            >checkbox</label
+                                        >
+                                    </div>
+                                </td>
+                                <th
+                                    scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black"
+                                >
+                                    Microsoft Surface Pro
+                                </th>
+                                <td class="px-6 py-4">White</td>
+                                <td class="px-6 py-4">Laptop PC</td>
+                                <td class="px-6 py-4">
+                                    <a
+                                        href="#"
+                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                        >Edit</a
+                                    >
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
